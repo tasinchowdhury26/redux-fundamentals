e31d8620de62f4705b70e77a94ef82df30166b36
tasinchowdhury26 <tasinchowdhury26@gmail.com>
2023-02-19 22:53:18 +0600

    managed states by lifting up, without redux

--- src/App.js
+++ src/App.js
@@ -1,7 +1,57 @@
+import { useState } from "react";
 import "./App.css";
 import Counter from "./components/Counter";
+import Stats from "./components/Stats";
+
+const initialState = [
+  {
+    id: 1,
+    count: 0,
+  },
+  {
+    id: 2,
+    count: 0,
+  },
+];
 
 function App() {
+  const [state, setState] = useState(initialState);
+
+  const totalCount = () => {
+    return state.reduce((prev, current) => prev + current.count, 0);
+  };
+
+  const increment = (id) => {
+    const updatedCounter = state.map((c) => {
+      if (c.id === id) {
+        return {
+          ...c,
+          count: c.count + 1,
+        };
+      } else {
+        return {
+          ...c,
+        };
+      }
+    });
+    setState(updatedCounter);
+  };
+  const decrement = (id) => {
+    const updatedCounter = state.map((c) => {
+      if (c.id === id) {
+        return {
+          ...c,
+          count: c.count - 1,
+        };
+      } else {
+        return {
+          ...c,
+        };
+      }
+    });
+    setState(updatedCounter);
+  };
+
   return (
     <div className="w-screen h-screen p-10 bg-gray-100 text-slate-700">
       <h1 className="max-w-md mx-auto text-center text-2xl font-bold">
@@ -9,9 +59,16 @@ function App() {
       </h1>
 
       <div className="max-w-md mx-auto mt-10 space-y-5">
-        <Counter></Counter>
-        <Counter></Counter>
-        <Counter></Counter>
+        {state.map((count) => (
+          <Counter
+            key={count.id}
+            id={count.id}
+            count={count.count}
+            increment={increment}
+            decrement={decrement}
+          />
+        ))}
+        <Stats count={totalCount()}></Stats>
       </div>
     </div>
   );
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,25 +1,13 @@
-import React, { useState } from "react";
+import Button from "./Button";
+import Count from "./Count";
 
-const Counter = () => {
-  const [count, setCount] = useState(0);
-
-  const increment = () => {
-    setCount(prev => prev + 1)
-  }
-  const decrement = () => {
-    setCount(prev => prev - 1)
-  }
-
+const Counter = ({id, count, increment, decrement}) => {
   return (
     <div className="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow">
-      <div className="text-2xl font-semibold">{count}</div>
+      <Count count={count} />
       <div className="flex space-x-3">
-        <button className="bg-indigo-400 text-white px-3 py-2 rounded shadow" onClick={increment}>
-          Increment
-        </button>
-        <button className="bg-red-400 text-white px-3 py-2 rounded shadow" onClick={decrement}>
-          Decrement
-        </button>
+        <Button handler={() => increment(id)}>Increment</Button>
+        <Button handler={() => decrement(id)}>Decrement</Button>
       </div>
     </div>
   );
